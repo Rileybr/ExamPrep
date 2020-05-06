dca440d3b01d0fb2a668e415b9e3c9d9a34dfd7e
rileybr <rileybr@ssfs.org>
2020-05-06 14:05:09 -0400

    String Manip Review ap review problem

---
+++ src/com/company/StringManip.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class StringManip
+{
+    /** Takes a string str and returns a new string * with all spaces removed.
+     */
+    public static String removeSpaces(String str)
+    {
+        String noSpace = "";
+        for (int i=0; i<str.length(); i++) {
+            if (!str.substring(i, i+1).equals(" ")) {
+                noSpace += str.substring(i, i+1);
+            }
+        }
+        return noSpace;
+    }
+
+
+    /** Takes a string str and returns a new string
+     * with the characters reversed, as described in part (a). */
+    public static String reverseString(String str)
+    { /* to be implemented in part (a) */
+        String reverse = "";
+        for (int i = str.length()-1; i >= 0; i--){
+            reverse += str.substring(i, i+1);
+        }
+        return reverse;
+    }
+
+
+    /** Determines whether str is a palindrome and prints a message
+     * indicating the result, as described in part (b).
+     * Precondition: str contains only lowercase letters and spaces.
+     */
+    public static void palindromeChecker(String str)
+    { /* to be implemented in part (b) */
+        if (removeSpaces(str).equals(reverseString(removeSpaces(str)))){
+            System.out.println(str + " is a palindrome");
+        }
+        else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+
+}
